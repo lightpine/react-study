56e92a61aa54cabf99903b2fdd85a755c0322b12
lightpine <photo.ms.choi@gmail.com>
2019-10-25 13:29:03 +0900

    Init더미 데이터 출력

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const init = [{
+  contents: "hello"
+},
+{
+  contents: "world"
+}]
 
 const Main: React.FC = () => {
+
+  const [todo, setTodo] = useState(init)
   return (
     <div>
-      Hello world!
+      <h1>Todo List</h1>
+      {todo.map((todo) => todo.contents)}
     </div>
   );
 }
