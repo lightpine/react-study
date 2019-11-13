33c7a903907bbf7e9597b37509a8be90fd78de45
Choi myeongsong <photo.ms.choi@gmail.com>
2019-11-13 18:44:49 +0900

    페이지 구성

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Main: React.FC = () => {
   return (
     <div>
-      Hello world!
+      <div>navi</div>
+      <div>cover</div>
+      <div>contents</div>
+      <div>contact</div>
     </div>
   );
 }
