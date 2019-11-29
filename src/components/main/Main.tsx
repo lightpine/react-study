6a9e0bafa72838390bfb84f01add33273ad56fe0
lightpine <photo.ms.choi@gmail.com>
2019-11-29 12:10:52 +0900

    프로젝트 셋팅 및 이미지 박스 생성

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Img from '../Img/italy01.png'
+import styled from 'styled-components'
 
 const Main: React.FC = () => {
   return (
-    <div>
-      Hello world!
-    </div>
+    <ImgBox />
   );
 }
+const ImgBox = styled.div`
+  background-image: url(${Img});
+  background-position: center center;
+  background-repeat: no-repeat;
+  height:300px;
+`
 
 export default Main
