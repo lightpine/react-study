8fcf2a907d789b433e69a4f57d3fca4df56afd50
lightpine <photo.ms.choi@gmail.com>
2019-11-29 14:08:26 +0900

    dot 파일 메인해 삽입 후 화면 조정

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,21 +1,42 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Img from '../Img/italy01.png'
 import styled from 'styled-components'
-import Dot from './Dots'
 
 const Main: React.FC = () => {
+
   return (
-    <Fragment>
+    <Wapper>
       <ImgBox />
-      <Dot />
-    </Fragment>
+      <Dot col="5" />
+      <Dot col="6" />
+      <Dot col="7" />
+    </Wapper>
   );
 }
+const Wapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12 1fr);
+  grid-auto-rows: auto;
+  grid-gap: 1rem;
+`
 const ImgBox = styled.div`
+  grid-column: 1 / 13;
   background-image: url(${Img});
   background-position: center center;
   background-repeat: no-repeat;
-  height:300px;
+  width: 1000px;
+  height: 300px;
+`
+const Dot = styled.div<{ col: string }>`
+  display: grid;
+  grid-column: ${props => props.col};
+  height: 10px;
+  width: 10px;
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
 `
 
 export default Main
