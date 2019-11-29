91f89aca056f9af2e32d01d2818bfe7b0e977b78
lightpine <photo.ms.choi@gmail.com>
2019-11-29 15:36:12 +0900

    타임아웃 걸어서 이미지 바꾸기

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Img from '../Img/italy01.png'
+import React, { useState, useEffect } from 'react'
+import Img1 from '../Img/italy01.png'
+import Img2 from '../Img/italy02.png'
+import Img3 from '../Img/italy03.png'
 import styled from 'styled-components'
 
-const Main: React.FC = () => {
+const Main: React.FC = (props) => {
+  const Imgs = [Img1, Img2, Img3]
+  const [imgNum, setImgNum] = useState(0)
+
+  useEffect(() => {
+    for (let i = 0; i <= Imgs.length; i++) {
+      setTimeout(() => {
+        setImgNum(i)
+        console.log("111", imgNum);
+      }, 2000);
+    }
+    console.log("222", imgNum);
+  }, [imgNum])
+
 
   return (
     <Wapper>
-      <ImgBox />
+      {/* <ImgBox img={image} /> */}
+      <ImgBox2 src={Imgs[imgNum]} />
       <Dot col="5" />
       <Dot col="6" />
       <Dot col="7" />
@@ -18,15 +34,22 @@ const Wapper = styled.div`
   grid-template-columns: repeat(12 1fr);
   grid-auto-rows: auto;
   grid-gap: 1rem;
+  justify-content: center;
 `
-const ImgBox = styled.div`
+
+const ImgBox2 = styled.img`
   grid-column: 1 / 13;
-  background-image: url(${Img});
-  background-position: center center;
-  background-repeat: no-repeat;
-  width: 1000px;
+  width: 500px;
   height: 300px;
 `
+// const ImgBox = styled.div<{ img: string }>`
+//   grid-column: 1 / 13;
+//   background-position: center center;
+//   background-image: url(${img});
+//   background-repeat: no-repeat;
+//   width: 1000px;
+//   height: 300px;
+// `
 const Dot = styled.div<{ col: string }>`
   display: grid;
   grid-column: ${props => props.col};
